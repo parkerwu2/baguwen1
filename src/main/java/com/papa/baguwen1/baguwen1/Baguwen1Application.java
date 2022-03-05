a908b9d7392e04d8076005690f92995f7cbf6604
parkerwu2 <982051137@qq.com>
2022-03-05 15:35:36 +0800

    Initial commit

---
+++ src/main/java/com/papa/baguwen1/baguwen1/Baguwen1Application.java
@@ -0,0 +1,13 @@
+package com.papa.baguwen1.baguwen1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baguwen1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baguwen1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/papa/baguwen1/baguwen1/Baguwen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papa.baguwen1.baguwen1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baguwen1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
