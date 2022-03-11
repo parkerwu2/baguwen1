f053e4ae6db0763e2ecc127c6a810d2f3a96533e
parkerwu2 <982051137@qq.com>
2022-03-11 14:36:13 +0800

    elasticjob

---
+++ src/main/java/com/papa/baguwen1/baguwen1/elasticjob/ElasticJobRegistryCenterConfig.java
@@ -0,0 +1,29 @@
+package com.papa.baguwen1.baguwen1.elasticjob;
+
+import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticJobRegistryCenterConfig {
+
+    //zookeeper链接字符串 localhost:2181
+    private  String ZOOKEEPER_CONNECTION_STRING = "127.0.0.1:2181" ;
+    //定时任务命名空间
+    private  String JOB_NAMESPACE = "elastic-job-boot-java";
+
+    //zk的配置及创建注册中心
+    @Bean(initMethod = "init")
+    public CoordinatorRegistryCenter setUpRegistryCenter(){
+        //zk的配置
+        ZookeeperConfiguration zookeeperConfiguration = new ZookeeperConfiguration(ZOOKEEPER_CONNECTION_STRING, JOB_NAMESPACE);
+
+        zookeeperConfiguration.setSessionTimeoutMilliseconds(1000);
+        //创建注册中心
+        CoordinatorRegistryCenter zookeeperRegistryCenter = new ZookeeperRegistryCenter(zookeeperConfiguration);
+        return zookeeperRegistryCenter;
+
+    }
+}
--- src/test/java/com/papa/baguwen1/baguwen1/JavaBootTest.java
+++ src/test/java/com/papa/baguwen1/baguwen1/JavaBootTest.java
@@ -1,5 +1,6 @@
 package com.papa.baguwen1.baguwen1;
 
+import com.papa.baguwen1.baguwen1.elasticjob.FileCustom;
 import com.papa.baguwen1.baguwen1.importor.TestA;
 import com.papa.baguwen1.baguwen1.importor.TestB;
 import com.papa.baguwen1.baguwen1.importor.TestC;
@@ -7,6 +8,10 @@ import com.papa.baguwen1.baguwen1.importor.TestD;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class JavaBootTest {
@@ -18,6 +23,8 @@ public class JavaBootTest {
     private TestC testC;
     @Autowired
     private TestD testD;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     public void test(){
@@ -25,5 +32,36 @@ public class JavaBootTest {
         testB.printName();
         testC.printName();
         testD.printName();
+    }
+
+    @Test
+    public void testGenerateTestData(){
+        //清除数据
+        clearTestFiles();
+        //制造数据
+        generateTestFiles();
+    }
+    /**
+     * 清除模拟数据
+     */
+    public void clearTestFiles(){
+        jdbcTemplate.update("delete from t_file");
+    }
+
+    /**
+     * 创建模拟数据
+     */
+    public void generateTestFiles(){
+        List<FileCustom> files =new ArrayList<>();
+        for(int i=1;i<11;i++){
+            files.add(new FileCustom(String.valueOf(i),"文件"+ i,"text","content"+ i));
+            files.add(new FileCustom(String.valueOf((i+10)),"文件"+(i+10),"image","content"+ (i+10)));
+            files.add(new FileCustom(String.valueOf((i+20)),"文件"+(i+20),"radio","content"+ (i+20)));
+            files.add(new FileCustom(String.valueOf((i+30)),"文件"+(i+30),"vedio","content"+ (i+30)));
+        }
+        for(FileCustom file : files){
+            jdbcTemplate.update("insert into t_file (id,name,type,content,backedUp) values (?,?,?,?,?)",
+                    new Object[]{file.getId(),file.getName(),file.getType(),file.getContent(),file.getBackedUp()});
+        }
     }
 }
