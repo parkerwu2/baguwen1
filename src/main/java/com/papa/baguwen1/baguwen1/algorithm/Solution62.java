881245b131a2de79a5e236f600bb32b1c615c2db
parkerwu2 <982051137@qq.com>
2022-04-03 17:48:30 +0800

    62. 不同路径

---
+++ src/main/java/com/papa/baguwen1/baguwen1/algorithm/Solution62.java
@@ -0,0 +1,19 @@
+package com.papa.baguwen1.baguwen1.algorithm;
+
+public class Solution62 {
+    public int uniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; i++){
+            f[i][0] = 1;
+        }
+        for (int i = 0 ; i < n; i++){
+            f[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+}
