a9ada67549f891655d289cf647ff4659a5fe30f2
parkerwu2 <982051137@qq.com>
2022-05-22 13:20:17 +0800

    函数式编程

--- src/main/java/com/papa/baguwen1/baguwen1/lambda/TestFunction.java
+++ src/main/java/com/papa/baguwen1/baguwen1/lambda/TestFunction.java
@@ -1,8 +1,13 @@
 package com.papa.baguwen1.baguwen1.lambda;
 
+import java.util.function.Function;
+
 public class TestFunction {
     private static String test(MyFunction<String> mf, String c){
         return  mf.getValue(c);
+    }
+    private static String test3(Function<String, String> mf, String c){
+        return  mf.apply(c);
     }
     private static Integer test2(MyFunction<Integer> mf, Integer c){
         return  mf.getValue(c);
@@ -11,5 +16,6 @@ public class TestFunction {
         System.out.println(test(s -> s.toUpperCase(), "abc"));
         MyFunction<Integer> mf2 = i -> i * i;
         System.out.println(test2(mf2, 2));
+        System.out.println(test3(s -> s.toUpperCase(), "abc"));
     }
 }
